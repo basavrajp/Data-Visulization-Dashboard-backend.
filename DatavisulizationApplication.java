ad7d21e134f116fc451ec34f655637276eac3667
basavrajp <basavrajpatil514@gmail.com>
2024-05-29 20:34:19 +0530

    Add files via upload

---
+++ DatavisulizationApplication.java
@@ -0,0 +1,13 @@
+package com.io.javaproject.datavisulization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavisulizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatavisulizationApplication.class, args);
+	}
+
+}
